704c114833197834f4a807609f3f736f963f7cdf
Abhishek Singh <abhisheksingh.g07@gmail.com>
2023-12-03 17:29:03 +0530

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // telling tailwind in which type of files tailwind can be used
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
